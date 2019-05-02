665e9e08e8e19cc65c1ddf844dce1ca695bc0e17
Abhijit Chatterjee <admin@abhijitchatterjee.com>
2019-05-02 16:23:56 +0100

    Added SCSS Generator, Cleaned up DOM for Custom Elements

--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@ import { LitElement, html, css } from 'lit-element';
 import './card/retro-box-card.js';
 import './layout/retro-box-layout';
 import './icon/retro-box-icon';
+import './topBar/retro-box-top-bar';
+
 import Logo from './assets/AIB_Logo.jpg';
 
 class RetroBox extends LitElement {
 
   static get styles() {
     return css`
-      :host{
-        background-color: red;
-      }
+     /* Custom CSS */
     `
   }
 
@@ -20,10 +20,13 @@ class RetroBox extends LitElement {
     this.colNames = JSON.stringify([{ colNames: 'Start' }, { colNames: 'Continue' }, { colNames: 'Stop' }]);
     this.logo = Logo;
 
+    this.title = 'Team Retrospective Box';
+    this.variant = 'dense-fixed-adjust';
+
     this.addCard = (e) => {
       const elem = e.target;
       const event = new CustomEvent('addCard', {
-        bubbles:true,
+        bubbles: true,
         composed: true,
         detail: {
           target: e.target.slot,
@@ -38,7 +41,9 @@ class RetroBox extends LitElement {
 
   render() {
     return html`
-      <retro-box-layout colNames='${this.colNames}' title='Team Retrospective Box' logo='${this.logo}'>
+
+      
+      <retro-box-layout colNames='${this.colNames}'>
         <retro-box-icon slot='Start' icon='add_circle_outline' @click='${this.addCard}'></retro-box-icon>
         <retro-box-icon slot='Continue' icon='add_circle_outline' @click='${this.addCard}'></retro-box-icon>
         <retro-box-icon slot='Stop' icon='add_circle_outline' @click='${this.addCard}'></retro-box-icon>
